6b55b597fc9002b7616469a39a6b1c190701cc4c
milindshyam95 <milindshyam95@gmail.com>
2024-10-23 22:06:36 +0530

    updated README and fetching data from local file as api was giving errors

--- src/features/inventoryData/inventoryDataApi.ts
+++ src/features/inventoryData/inventoryDataApi.ts
@@ -3,6 +3,8 @@ import axios from "axios"
 export const fetchInventoryData = async () => {
     try{
         // const response = await axios.get('https://dev-0tf0hinghgjl39z.api.raw-labs.com/inventory')
+
+        //fetching data from local file as the api was constatly giving errors. Tried on Postman as well. 
         const response = await axios.get('/data.json')
         return response
     }
